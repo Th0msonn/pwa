775f72633d61ed26dff7ed0111fc5a02f7cd1ddd
Thomas Dalphinet <thomasdalphinet@MacBook-Pro-de-Thomas-2.local>
2019-09-17 09:52:18 +0200

    JS et manifest

---
+++ app.js
@@ -0,0 +1,29 @@
+//let bouton_menu = document.querySelector(".burger");
+//let menu = document.querySelector("header nav");
+
+document.addEventListener("click", afficheMenu);
+
+function afficheMenu() {
+    var menu = document.querySelector('header nav')
+
+    //console.log(event.target.matches("header"));
+
+    //if (!event.target.matches('.burger') && !event.target.matches('header nav')) {
+    //console.log("ce n'est pas un burger et nav");
+    //if (menu.classList.contains('active')) {
+    // menu.classList.remove('active')
+    // }
+    // return;
+    // }
+    // menu.classList.add('active');
+
+    if (event.target.matches(".burger") || event.target.matches("header nav")) {
+        menu.classList.add('active');
+    }
+    else {
+        menu.classList.remove('active')
+    }
+
+};
+
+
